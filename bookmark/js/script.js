f863502bd4dc755dbb1af440d1fcb15bd3e156d4
Kalzang dorje lama <kalzanglama77@gmail.com>
2025-11-02 21:48:52 +0545

    bookmark landing page completed

--- bookmark/js/script.js
+++ bookmark/js/script.js
@@ -20,7 +20,10 @@ const featureContent = [
 ];
 
 const featureHeading = document.querySelectorAll('.feature-heading');
-const activateContentFeatures = document.getElementById('activate-content-features');
+
+const activateContentFeatures = document.getElementById('activateContentFeatures');
+// console.log("activateContentFeatures :",activateContentFeatures)
+
 const orangeActiveLine = document.querySelectorAll('.orange-active-line')
 // console.log(orangeActiveLine)
 
@@ -59,39 +62,57 @@ featureHeading.forEach((heading, index) => {
 const accrodianContent = [
     {
         question: 'What is Bookmark?',
-        answer: 'Bookmark is a lightweight browser extension that lets you organize, save, and manage your favorite websites in one place. It helps you quickly access and categorize bookmarks for faster browsing.'
+        answer: 'Bookmark is a lightweight browser extension that lets you organize, save, and manage your favorite websites in one place. It helps you quickly access and categorize bookmarks for faster browsing.',
+        img: './images/icon-arrow.svg'
     },
     {
         question: 'How can I request a new browser?',
-        answer: 'If your browser isn’t supported yet, you can send us a feature request through our contact form. We’re always open to adding support for more browsers based on user demand.'
+        answer: 'If your browser isn’t supported yet, you can send us a feature request through our contact form. We’re always open to adding support for more browsers based on user demand.',
+        img: './images/icon-arrow.svg'
     },
     {
         question: 'Is there a mobile app?',
-        answer: 'Currently, Bookmark is only available as a browser extension. However, we’re actively working on a mobile version that will let you sync your saved bookmarks across all your devices.'
+        answer: 'Currently, Bookmark is only available as a browser extension. However, we’re actively working on a mobile version that will let you sync your saved bookmarks across all your devices.',
+        img: './images/icon-arrow.svg'
     },
     {
         question: 'What about other Chromium browsers?',
-        answer: 'Bookmark works perfectly with most Chromium-based browsers like Microsoft Edge, Brave, and Opera. Just install it from your browser’s web store and you’re good to go.'
+        answer: 'Bookmark works perfectly with most Chromium-based browsers like Microsoft Edge, Brave, and Opera. Just install it from your browser’s web store and you’re good to go.',
+        img: './images/icon-arrow.svg'
     }
 ];
-
 
 const accrodian = document.getElementById('accrodian');
 
 accrodian.innerHTML = '';
 
-accrodianContent.forEach((faq, index) => {
+accrodianContent.forEach((faq, i) => {
     const faqItem = document.createElement('div');
-    faqItem.classList.add('py-4', 'border-b', 'border-gray-300');
+    faqItem.classList.add('py-4', 'border-b', 'border-gray-300', 'flex', 'flex-col', 'space-y-4');
 
     faqItem.innerHTML = `
-    <p class="faq-question font-semibold cursor-pointer">${faq.question}</p>
-    <p class="faq-answer hidden text-gray-500 mt-2">${faq.answer}</p>
-    <div class="gray-line mt-4"></div>
-  `;
-
+        <div class="flex flex-col">
+            <div class="flex justify-between items-center">
+                <div class="faq-question font-semibold cursor-pointer mr-1 hover:text-softRed text-sm">${faq.question}</div>
+                <img src=${faq.img} alt="" class="arrow transition-transform duration-300">
+            </div>
+            <p class="faq-answer hidden text-gray-500">${faq.answer}</p>
+        </div>
+        <div class="white-line"></div>
+    `;
     faqItem.querySelector('.faq-question').addEventListener('click', () => {
         const answer = faqItem.querySelector('.faq-answer');
+        // console.log(answer)
+
+        const arrow = document.querySelectorAll('.arrow')
+        // console.log(arrow)
+
+        arrow.forEach((arrowUP, index) => {
+            console.log("index :", index)
+            if (index === i) {
+                arrowUP.classList.toggle('arrow-up')
+            }
+        })
         answer.classList.toggle('hidden');
     });
     accrodian.appendChild(faqItem);
@@ -99,3 +120,53 @@ accrodianContent.forEach((faq, index) => {
 
 
 
+const open = document.getElementById('open')
+// console.log(open)
+
+const whiteLogo = document.getElementById('white-logo')
+// console.log(whiteLogo)
+
+const hamburgerContentOpen = document.getElementById('hamburger-content-open')
+// console.log(hamburgerContentOpen)
+
+const hamburger = document.getElementById('hamburger')
+// console.log(hamburger)
+
+open.addEventListener('click', () => {
+    const isOpen = open.classList.toggle('open')
+    open.classList.toggle('fixed')
+    // console.log(isOpen)
+
+    hamburger.classList.toggle('px-20')
+    // whiteLogo.classList.add('text-white')
+    hamburgerContentOpen.classList.toggle('hidden')
+
+    if (isOpen) {
+        logoImgDynamic.innerHTML = `
+            <img src="./images/logo-bookmark-footer.svg" alt="logo-bookmark.svg">
+        `
+    } else {
+        logoImgDynamic.innerHTML = `
+            <img src="./images/logo-bookmark.svg" alt="logo-bookmark.svg">
+        `
+    }
+})
+
+
+const links = document.querySelectorAll('.nav-link');
+
+links.forEach(link => {
+    link.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        const targetId = this.getAttribute('href');
+        console.log(targetId)
+
+        const targetSection = document.querySelector(targetId);
+        console.log(targetId)
+
+        targetSection.scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
---
+++ readme.md
@@ -0,0 +1,3 @@
+Loopstudios landing page  [Bookmark](https://690777ffd178da214ed97b4d--teal-dasik-158a08.netlify.app/)
+
+
